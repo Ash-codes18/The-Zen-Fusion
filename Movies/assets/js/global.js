7a837c01a28a9cd4783db3d4caae8131bd74fd2d
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-04-18 22:19:42 +0530

    update 1 file and delete 2 files

--- Movies/assets/js/global.js
+++ Movies/assets/js/global.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// ! ========== ADD EVENT ON MULTIPLE ELEMENTS ==========
 
 const addEventOnElements = function (elements, eventType, callback) {
     elements.forEach((elem) => {
@@ -8,7 +7,6 @@ const addEventOnElements = function (elements, eventType, callback) {
     });
 }
 
-// ! ========== TOGGLE SEARCH BOX IN SMALL DEVICES ==========
 
 const searchBox = document.querySelector('[search-box]');
 const searchTogglers = document.querySelectorAll('[search-toggler]');
@@ -16,17 +14,11 @@ addEventOnElements(searchTogglers, 'click', function () {
     searchBox.classList.toggle('active');
 });
 
-/**
- * ! store movieId in 'localStorage' when you click any movie card
- */
 
 const getMovieDetail = function (movieId) {
     window.localStorage.setItem('movieId', String(movieId));
 }
 
-/**
- * ! store urlParam & genreName in 'localStorage' when you click any sidebar link
- */
 
 const getMovieList = function (urlParam, genreName) {
     window.localStorage.setItem('urlParam', urlParam);
--- Music/api.md
+++
@@ -1,4 +0,0 @@
-url : https://music-api-sigma-one.vercel.app/docs#tag/search/get/api/search
-
-
-supabase : https://www.youtube.com/watch?v=mpBaBJEcg04&feature=youtu.be
